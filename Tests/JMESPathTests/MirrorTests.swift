903c5b3133f3b0cec9d287b6142593d89a867cc6
Adam Fowler <adamfowler71@gmail.com>
2021-05-31 11:23:49 +0100

    Add Mirror tests

---
+++ Tests/JMESPathTests/MirrorTests.swift
@@ -0,0 +1,65 @@
+@testable import JMESPath
+import XCTest
+
+final class MirrorTests: XCTestCase {
+    func testInterpreter<Value: Equatable>(_ expression: String, data: Any, result: Value) {
+        do {
+            let expression = try Expression.compile(expression)
+            let value = try XCTUnwrap(expression.search(data))
+            XCTAssertEqual(value as? Value, result)
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+
+    func testString() {
+        struct TestString {
+            let s: String
+        }
+        let test = TestString(s: "hello")
+        self.testInterpreter("s", data: test, result: "hello")
+    }
+
+    func testOptional() {
+        struct TestString {
+            let s: String?
+        }
+        let test = TestString(s: "hello")
+        self.testInterpreter("s", data: test, result: "hello")
+    }
+
+    func testNumbers() {
+        struct TestNumbers {
+            let i: Int
+            let d: Double
+            let f: Float
+            let b: Bool
+        }
+        let test = TestNumbers(i:34, d: 1.4, f: 2.5, b: true)
+        self.testInterpreter("i", data: test, result: 34)
+        self.testInterpreter("d", data: test, result: 1.4)
+        self.testInterpreter("f", data: test, result: 2.5)
+        self.testInterpreter("b", data: test, result: true)
+    }
+
+    func testArray() {
+        struct TestArray {
+            let a: [Int]
+        }
+        let test = TestArray(a: [1,2,3,4,5])
+        self.testInterpreter("a[2]", data: test, result: 3)
+        self.testInterpreter("a[-2]", data: test, result: 4)
+        self.testInterpreter("a[1]", data: test, result: 2)
+    }
+
+    func testObjects() {
+        struct TestObject {
+            struct TestSubObject {
+                let a: String
+            }
+            let sub: TestSubObject
+        }
+        let test = TestObject(sub: .init(a: "hello"))
+        self.testInterpreter("sub.a", data: test, result: "hello")
+    }
+}

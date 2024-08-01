bcbfdc019bf01986dbc117778f7c57b2a993f0cd
Adam Fowler <adamfowler71@gmail.com>
2024-08-01 13:42:00 +0000

    Change bool check

--- Sources/JMESPath/Variable.swift
+++ Sources/JMESPath/Variable.swift
@@ -26,7 +26,7 @@ public enum JMESVariable {
         case let number as NSNumber:
             // both booleans and integer/float point types can be converted to a `NSNumber`
             // We have to check to see the type id to see if it is a boolean
-            if UInt8(number.objCType[0]) == UInt8(ascii: "c") {
+            if type(of: number) == Self.nsNumberBoolType {
                 self = .boolean(number.boolValue)
             } else {
                 self = .number(number)
@@ -266,6 +266,8 @@ public enum JMESVariable {
         guard let first = mirror.children.first else { return nil }
         return first.value
     }
+
+    fileprivate static var nsNumberBoolType = type(of: NSNumber(value: true))
 }
 
 extension JMESVariable: Equatable {
